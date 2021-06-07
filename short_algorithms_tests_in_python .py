b38d56cd7aac0a9229e2d04e42c4b9f60687ba25
germandus <germandouce@gmail.com>
2021-06-07 20:50:05 -0300

    .join y print

--- short_algorithms_tests_in_python .py
+++ short_algorithms_tests_in_python .py
@@ -1003,3 +1003,26 @@ def imprimo(tupla):
 l =list()
 imprimo ( (l,2) )
 '''
+#practica simple con paaremtro sep de print
+'''
+cadena = 'hola'
+
+for letra in cadena:
+    print(letra,'_', sep ='', end='')
+'''
+#VALIDADOR DE OOPCIONES SUPER UTIL XA PARCIAL (y xa todo en realidad)!!!
+'''
+def validar_opcion(opc_minimas: int, opc_maximas: int, texto: str = '') -> str:
+    """
+    PRE: "opc_minimas" y "opc_maximas" son dos números enteros que 
+    simbolizan la cantidad de opciones posibles.
+
+    POST: Devuelve en formato string la var "opc" con un número 
+    entero dentro del rango de opciones.
+    """
+    opc = input("{}".format(texto))
+    while not opc.isnumeric() or int(opc) > opc_maximas or int(opc) < opc_minimas:
+        opc = input("Por favor, ingrese una opcion valida: ")
+    
+    return opc
+'''
